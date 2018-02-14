ced927813ae7f325bf5a2afcedd643933d9bfbb7
Saksham Jain <saksham.jain1398@gmail.com>
2018-02-14 14:24:04 +0530

    Saved multiple requests to champions page and items for champions. Also grouped all the items according to their champion id in champion.js

--- routes/champions.js
+++ routes/champions.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 var Promise = require('promise');
 var tempdb = require('./tempdb');
-
+var items_map=new Map();
+var data;
 router.get('/', function(req, res, next) {
 
     var sessionId = tempdb.getSessionId();
@@ -11,27 +12,49 @@ router.get('/', function(req, res, next) {
     //console.log('a ' +sessionId);
     //console.log(pal);
 
-    var data = [];
 
-    var newPromise = new Promise(function (resolve,reject) {
-        pal.getChampions(sessionId, 'PC', (err, res) => {
-            //console.log(res);
-            resolve(res);
-        });
-    })
-    .then(function (result) {
-        data = result;
-        // res.send(data);
-        var newPromise1=new Promise(function (resolve,reject) {
-            pal.getItems(sessionId,'PC',(err,res)=>{
+    if(data==undefined) {
+        var newPromise = new Promise(function (resolve, reject) {
+            pal.getChampions(sessionId, 'PC', function (err, res) {
+                //console.log(res);
+                console.log("CHAMP REQ");
                 resolve(res);
             });
         })
-            .then(function (result1) {
-                // console.log(result1);
-                res.render('champions',{data:data,items:result1});
-            })
-    });
+            .then(function (result) {
+                data = result;
+                // res.send(data);
+                if (items_map.size == 0) {
+                    var newPromise1 = new Promise(function (resolve, reject) {
+                        pal.getItems(sessionId, 'PC', function (err, res) {
+                            console.log("ITEMS REQ");
+                            resolve(res);
+                        });
+                    })
+                        .then(function (result1) {
+                            // console.log(result1);
+                            result1.forEach(function (item) {
+                                var temp = items_map.get(item.champion_id);
+                                if (temp == undefined) {
+                                    temp = new Set();
+                                    temp.add(item);
+                                    items_map.set(item.champion_id, temp);
+                                }
+                                else {
+                                    items_map.set(item.champion_id, temp.add(item));
+                                }
+                            });
+                            res.render('champions', {data: data, items_map: items_map});
+                        })
+                }
+                else {
+                    res.render('champions', {data: data, items_map: items_map});
+                }
+            });
+    }
+    else{
+        res.render('champions', {data: data, items_map: items_map});
+    }
 });
 
 module.exports = router;
